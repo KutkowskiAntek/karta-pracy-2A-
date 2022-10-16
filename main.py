869f0e0608378018453259572e64358c08769f1a
i23456789o <>
2022-10-16 17:19:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+# zad 1
+import math
+
+# a, b = int(input()), int(input())
+
+# if (a+b)%2==0:
+#       print("tak")
+# else:
+#       print("nie")
+
+# zad 2
+
+# a, g = int(input()), int(input())
+
+# if (a+g)/2>math.sqrt(a*g):
+#     print("tak")
+# else:
+#     print("nie")
+
+# zad 3
+
+
+# k = int(input())
+# l = int(input())
+# m = int(input())
+# 
+# if (k==l and m!=k and m!=l):
+#     print("tak równe są ", k, "i", l)
+# else:
+#     if (k!=l and m==k and m!=l):
+#         print("tak, równe są ", m,"i", k)
+#     else:
+#         if (k!=l and m!=k and m==l):
+#             print("tak, równe są ", m,"i", l)
+#         else:
+#             print("nie, żadna z nich nie jest równa lub wszystkie 3 są równe")
+
+# zad 4
+
+
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+#
+# if a<b and a<c and a<d:
+#     print(a)
+# else:
+#     if b<a and b<c and b<d:
+#         print(b)
+#     else:
+#         if c<a and c<b and c<d:
+#             print(c)
+#         else:
+#             if d<a and d<b and d<c:
+#                 print(d)
+#                 pass
+
+# zad 5
+
+
+# a, b, c = int(input()), int(input()), int(input())
+#
+# if a<b+c and b<a+c and c<a+b:
+#     print("tak, da się zbudować trójkąt")
+# else:
+#     print("nie, nie da się zbudować trójkąta")
+
+# zad 6
+
+#
+# a, b, c = int(input()), int(input()), int(input())
+#
+# if (a*a+b*b == c*c) or (a*a+c*c == b*b) or (c*c+b*b == a*a):
+#     print("prostokątny")
+# else:
+#     if (a*a+b*b < c*c) or (a*a+c*c < b*b) or (c*c+b*b < a*a):
+#         print("rozwartokątny")
+#     else:
+#         if (a*a+b*b > c*c) or (a*a+c*c > b*b) or (c*c+b*b > a*a):
+#             print("ostrokątny")
+#             pass
